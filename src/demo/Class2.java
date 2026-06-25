2ca4546830902ae9151d92d377ce8e901e9d2e18
Keertana <keertana.krishnappa@outlook.com>
2026-06-26 01:44:10 +0530

    1st person day2 commit

---
+++ src/demo/Class2.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I got selected in infosys interview");
+	}
+
+}
